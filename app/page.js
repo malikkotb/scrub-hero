beac9e7367fc6a46117c200d2f48defd25d3cdbf
malik <malikkotb@icloud.com>
2024-06-18 08:52:23 +0200

    setup

---
+++ app/page.js
@@ -0,0 +1,29 @@
+export default function Home() {
+  return (
+    <>
+      {/* <section></section> */}
+      <section className="primary">
+        <aside>
+          <div className="mask">
+            <h1>diabolical</h1>
+          </div>
+          <div className="mask">
+            <p>process</p>
+          </div>
+        </aside>
+        <main>
+          <div className="mask">
+            <p>
+              The processes utilized in our next-gen system are diabolical in nature. What does this mean? You’ll have
+              to find out.
+            </p>
+          </div>
+        </main>
+
+        <div className="line"></div>
+        <span>diabolical</span>
+      </section>
+      <section></section>
+    </>
+  );
+}
